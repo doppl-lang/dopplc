9bacfd21c5090c5cfe7d54a76ee0f9436771f192
Diego PERINI <diegoperini2886@gmail.com>
2015-11-26 03:32:02 +0200

    Cleaned project dependencies from repo (downloaded at runtime)

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 clear
-rm bin/*
+rm -rf bin
 coffee -b --output bin --compile src
 cd src
 jison doppl_grammar.jison 
 mv doppl_grammar.js ../bin/doppl_grammar.js
 cp doppl.cpp.mustache ../bin/doppl.cpp.mustache
 cp state_bodies.mustache ../bin/state_bodies.mustache
-cd ../bin
+cd ..
+npm i
+cd bin
 node dopplc.js ../$1
+cd ..
+rm -rf node_modules
 
